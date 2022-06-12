8ea0ef6c00e794768a405d0db45c6cb3360ec513
t <t@t.com>
2022-06-13 00:22:54 +0200

    fix product_name

--- routes/pi.js
+++ routes/pi.js
@@ -63,7 +63,7 @@ router.post('/insert_package', async (req, res) => {
       token: user.fcmToken,
       notification: { 
         title: "Your package has been delivered to the locker 🎉",
-        body: `Your order of ${product.name} has been successfully delivered to the chosen locker. You can collect it now.`,
+        body: `Your order of ${product.product_name} has been successfully delivered to the chosen locker. You can collect it now.`,
         imageUrl: "https://locker.fekry.dev/images/n.jpg"
       }
     });
